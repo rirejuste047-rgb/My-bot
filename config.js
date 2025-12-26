03e4fc8203aed7cbd93e507c7191db3fb7c3fc2e
rirejuste047-rgb <rirejuste047@gmail.com>
2025-12-26 07:27:24 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export default {
+  BotName: "DofuBot", // Bot name
+  nameCreator: "オビト・", // Your name
+  Newsletter: "120363401981326696@newsletter", // Newsletter for your channel
+  Newsletter2: "newsletter from your channel", // Optional
+  Newsletter3: "Newsletter for your channel", // Optional
+  Newsletter4: "Newsletter for your channel", // Optional
+  Newsletter5: "Newsletter for your channel", // Optional
+  Newsletter6: "Newsletter for your channel", // Optional
+};
